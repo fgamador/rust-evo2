598ab7a16ccaa9df9983a9a16504a904be635993
Franz Amador <franzamador@gmail.com>
2022-01-21 11:51:41 -0800

    r Extract fns

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 fn main() {
     loop {
-        let (num_created, num_died) = (0, 0);
-        let num_alive = 100;
+        let (num_created, num_died) = step();
+        let num_alive = num_alive();
         println!("+{} -{} -> {}", num_created, num_died, num_alive);
     }
+}
+
+fn num_alive() -> i32 {
+    100
+}
+
+fn step() -> (i32, i32) {
+    (0, 0)
 }
