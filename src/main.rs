471190fcf03e6abecdb1e58e4b2e0a617aaae4b7
Franz Amador <franzamador@gmail.com>
2022-01-21 11:43:08 -0800

    R!! Group scalar vars into tuple

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 fn main() {
     loop {
-        let num_created = 0;
-        let num_died = 0;
+        let (num_created, num_died) = (0, 0);
         let num_alive = 100;
         println!("+{} -{} -> {}", num_created, num_died, num_alive);
     }
