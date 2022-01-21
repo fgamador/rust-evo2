a627837b98837323fb36a1f8a164238cf6130dff
Franz Amador <franzamador@gmail.com>
2022-01-21 11:40:05 -0800

    R!! Introduce vars

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
+    let num_created = 0;
+    let num_died = 0;
+    let num_alive = 100;
     loop {
-        println!("+0 -0 -> 100");
+        println!("+{} -{} -> {}", num_created, num_died, num_alive);
     }
 }
