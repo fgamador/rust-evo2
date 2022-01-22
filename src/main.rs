7574b398bdba8b5b59a65aedc3ba3f52d1daea5f
Franz Amador <franzamador@gmail.com>
2022-01-21 16:19:34 -0800

    F** Add hard-coded average energy to printed status

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ fn main() {
     let mut world = World::new();
     loop {
         let (num_created, num_died) = world.step();
-        println!("+{} -{} -> {}", num_created, num_died, world.num_alive());
+        println!("+{} -{} -> {} (e: {})", num_created, num_died, world.num_alive(),
+                 world.average_energy());
     }
 }
 
@@ -17,6 +18,10 @@ impl World {
 
     pub fn num_alive(&self) -> usize {
         self.cells.len()
+    }
+
+    pub fn average_energy(&self) -> f64 {
+        100.0
     }
 
     pub fn step(&mut self) -> (i32, i32) {
