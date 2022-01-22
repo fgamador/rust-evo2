5f6dc0579119e106f072d1307ad95822f09a6e24
Franz Amador <franzamador@gmail.com>
2022-01-21 19:22:50 -0800

    r Add unused param

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let mut world = World::new(100, 100.0);
+    let mut world = World::new(100, 100.0, CellParameters);
     loop {
         let (num_created, num_died) = world.step();
         println!("+{} -{} -> {} (e: {})", num_created, num_died, world.num_alive(),
@@ -12,7 +12,7 @@ pub struct World {
 }
 
 impl World {
-    pub fn new(num_cells: usize, initial_energy: f32) -> Self {
+    pub fn new(num_cells: usize, initial_energy: f32, _cell_params: CellParameters) -> Self {
         World {
             cells: vec![Cell { energy: initial_energy }; num_cells],
         }
@@ -36,19 +36,28 @@ pub struct Cell {
     pub energy: f32,
 }
 
+pub struct CellParameters;
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn world_cells_start_alive() {
-        let world = World::new(42, 100.0);
+        let world = World::new(42, 100.0, CellParameters);
         assert_eq!(world.num_alive(), 42);
     }
 
     #[test]
     fn average_energy_starts_at_initial_energy() {
-        let world = World::new(100, 39.5);
+        let world = World::new(100, 39.5, CellParameters);
         assert_eq!(world.average_energy(), 39.5);
     }
+
+    // #[test]
+    // fn cells_use_energy() {
+    //     let mut world = World::new(100, 10.0, 5.25);
+    //     world.step();
+    //     assert_eq!(world.average_energy(), 4.75);
+    // }
 }
