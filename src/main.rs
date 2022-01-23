c8fe575da1c1594965993c1674a41292e1d5cb25
Franz Amador <franzamador@gmail.com>
2022-01-22 16:02:05 -0800

    r Inline vars

--- src/main.rs
+++ src/main.rs
@@ -87,66 +87,59 @@ mod tests {
 
     #[test]
     fn world_cells_start_alive() {
-        let num_cells = 42;
-        let cell_params = CellParameters::DEFAULT;
-        let world = World::new(generate_cells(num_cells, cell_params));
+        let world = World::new(generate_cells(42, CellParameters::DEFAULT));
         assert_eq!(world.num_alive(), 42);
     }
 
     #[test]
     fn average_energy_starts_at_initial_energy() {
-        let num_cells = 100;
         let cell_params = CellParameters {
             initial_energy: 39.5,
             ..CellParameters::DEFAULT
         };
-        let world = World::new(generate_cells(num_cells, cell_params));
+        let world = World::new(generate_cells(100, cell_params));
         assert_eq!(world.average_energy(), 39.5);
     }
 
     #[test]
     fn average_energy_with_no_cells_is_zero() {
-        let num_cells = 0;
         let cell_params = CellParameters::DEFAULT;
-        let world = World::new(generate_cells(num_cells, cell_params));
+        let world = World::new(generate_cells(0, cell_params));
         assert_eq!(world.average_energy(), 0.0);
     }
 
     #[test]
     fn cells_use_energy() {
-        let num_cells = 100;
         let cell_params = CellParameters {
             initial_energy: 10.0,
             energy_use_per_step: 5.25,
             ..CellParameters::DEFAULT
         };
-        let mut world = World::new(generate_cells(num_cells, cell_params));
+        let mut world = World::new(generate_cells(100, cell_params));
         world.step();
         assert_eq!(world.average_energy(), 4.75);
     }
 
     #[test]
     fn dead_cells_disappear() {
-        let num_cells = 10;
         let cell_params = CellParameters {
             initial_energy: 10.0,
             energy_use_per_step: 11.0,
             ..CellParameters::DEFAULT
         };
-        let mut world = World::new(generate_cells(num_cells, cell_params));
+        let mut world = World::new(generate_cells(10, cell_params));
         world.step();
         assert_eq!(world.num_alive(), 0);
     }
 
     #[test]
     fn world_step_reports_num_died() {
-        let num_cells = 10;
         let cell_params = CellParameters {
             initial_energy: 10.0,
             energy_use_per_step: 5.0,
             ..CellParameters::DEFAULT
         };
-        let mut world = World::new(generate_cells(num_cells, cell_params));
+        let mut world = World::new(generate_cells(10, cell_params));
         let (_, num_died) = world.step();
         assert_eq!(num_died, 0);
         let (_, num_died) = world.step();
