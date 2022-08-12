e48e100246801d39f2a21937f577e2be5935cf14
Franz Amador <franzamador@gmail.com>
2022-08-12 13:55:02 -0700

    R!! simplify imports

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use clap::Parser;
-use evo2::main_support;
-use evo2::main_support::Args;
+use evo2::main_support::{create_and_run_world, Args};
 
 fn main() {
-    main_support::create_and_run_world(&Args::parse());
+    create_and_run_world(&Args::parse());
 }
