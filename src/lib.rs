b69efca675c3cc7fb2fd16624d1fb114b21180fe
Franz Amador <franzamador@gmail.com>
2022-08-13 15:41:43 -0700

    R** Add unused constrained number types

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod cell;
 pub mod food_sources;
 pub mod main_support;
+pub mod number_types;
 pub mod world;
---
+++ src/number_types.rs
@@ -0,0 +1,37 @@
+use std::convert::From;
+
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
+pub struct F32Positive {
+    value: f32,
+}
+
+impl From<F32Positive> for f32 {
+    fn from(num: F32Positive) -> Self {
+        num.value
+    }
+}
+
+impl From<f32> for F32Positive {
+    fn from(num: f32) -> Self {
+        assert!(num >= 0.0);
+        F32Positive { value: num }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
+pub struct F32ZeroToOne {
+    value: f32,
+}
+
+impl From<F32ZeroToOne> for f32 {
+    fn from(num: F32ZeroToOne) -> Self {
+        num.value
+    }
+}
+
+impl From<f32> for F32ZeroToOne {
+    fn from(num: f32) -> Self {
+        assert!(0.0 <= num && num <= 1.0);
+        F32ZeroToOne { value: num }
+    }
+}
