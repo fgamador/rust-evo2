2c99d0694f50cddeb28420b83da5af9699d2d10d
Franz Amador <franzamador@gmail.com>
2022-08-14 13:02:22 -0700

    R** Clarify/adjust F32ZeroToOne behavior

--- src/number_types.rs
+++ src/number_types.rs
@@ -11,15 +11,15 @@ pub struct F32Positive {
 impl F32Positive {
     pub fn checked(value: f32) -> Self {
         assert!(value >= 0.0);
-        F32Positive { value }
+        Self { value }
     }
 
     pub const fn unchecked(value: f32) -> Self {
-        F32Positive { value }
+        Self { value }
     }
 
     pub fn clipped(value: f32) -> Self {
-        F32Positive { value: value.max(0.0) }
+        Self { value: value.max(0.0) }
     }
 
     pub const fn value(&self) -> f32 {
@@ -29,31 +29,31 @@ impl F32Positive {
 
 impl fmt::Display for F32Positive {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.value)
+        write!(f, "{}", self.value())
     }
 }
 
 impl AddAssign for F32Positive {
     fn add_assign(&mut self, other: Self) {
-        *self = Self::unchecked(self.value() + other.value);
+        *self = Self::unchecked(self.value() + other.value());
     }
 }
 
 impl SubAssign for F32Positive {
     fn sub_assign(&mut self, other: Self) {
-        *self = Self::clipped(self.value() - other.value);
+        *self = Self::clipped(self.value() - other.value());
     }
 }
 
 impl From<F32Positive> for f32 {
     fn from(num: F32Positive) -> Self {
-        num.value
+        num.value()
     }
 }
 
 impl From<f32> for F32Positive {
     fn from(num: f32) -> Self {
-        F32Positive::checked(num)
+        Self::checked(num)
     }
 }
 
@@ -63,10 +63,17 @@ pub struct F32ZeroToOne {
 }
 
 impl F32ZeroToOne {
-    // TODO convert to macro with checking?
-    pub const fn unchecked(num: f32) -> Self {
-        // assert!(num >= 0.0);
-        F32ZeroToOne { value: num }
+    pub fn checked(value: f32) -> Self {
+        assert!((0.0..=1.0).contains(&value));
+        Self { value }
+    }
+
+    pub const fn unchecked(value: f32) -> Self {
+        Self { value }
+    }
+
+    pub fn clipped(value: f32) -> Self {
+        Self { value: value.max(0.0).min(1.0) }
     }
 
     pub const fn value(&self) -> f32 {
@@ -76,19 +83,18 @@ impl F32ZeroToOne {
 
 impl fmt::Display for F32ZeroToOne {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.value)
+        write!(f, "{}", self.value())
     }
 }
 
 impl From<F32ZeroToOne> for f32 {
     fn from(num: F32ZeroToOne) -> Self {
-        num.value
+        num.value()
     }
 }
 
 impl From<f32> for F32ZeroToOne {
     fn from(num: f32) -> Self {
-        assert!((0.0..=1.0).contains(&num));
-        F32ZeroToOne { value: num }
+        Self::checked(num)
     }
 }
