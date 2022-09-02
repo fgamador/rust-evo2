336435b517af0056cb1e5565430fa85b4f449680
Franz Amador <franzamador@gmail.com>
2022-09-02 16:55:58 -0700

    Adjust example

--- examples/malthus.rs
+++ examples/malthus.rs
@@ -2,14 +2,14 @@ use evo2::main_support::{create_and_run_world, Args};
 
 fn main() {
     create_and_run_world(&Args {
+        initial_food: 100.0,
         cells: 1,
         initial_energy_mean: 10.0,
-        initial_food: 100.0,
         attempted_eating_energy_mean: 1.0,
         food_yield_from_eating: 10.0,
         energy_yield_from_digestion: 1.0,
         child_threshold_energy_mean: 2.0,
-        create_child_energy: 2.0,
+        create_child_energy: 1.0,
         maintenance_energy: 1.0,
         ..Args::DEFAULT
     });
