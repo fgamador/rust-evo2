69b07636beec9d0680dfe6f6449c50c7ca9550f5
Franz Amador <franzamador@gmail.com>
2022-09-03 17:54:26 -0700

    Futzing with example

--- examples/eat_then_starve.rs
+++ examples/eat_then_starve.rs
@@ -9,8 +9,8 @@ fn main() {
         food_yield_from_eating: 10.0,
         energy_yield_from_digestion: 1.0,
         health_reduction_per_energy_expended: 0.1,
-        attempted_healing_energy_mean: 0.0,
-        health_increase_per_healing_energy: 0.0,
+        attempted_healing_energy_mean: 1.0,
+        health_increase_per_healing_energy: 0.5,
         ..Args::DEFAULT
     });
 }
