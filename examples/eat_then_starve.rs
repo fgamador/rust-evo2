ca25405808940cdf8daf8c3b75d54716a2377cd2
Franz Amador <franzamador@gmail.com>
2022-09-03 18:06:53 -0700

    Futzing with example

--- examples/eat_then_starve.rs
+++ examples/eat_then_starve.rs
@@ -5,6 +5,7 @@ fn main() {
         initial_food: 100.0,
         cells: 1,
         initial_energy_mean: 10.0,
+        health_reduction_from_entropy: 0.1,
         attempted_eating_energy_mean: 1.0,
         food_yield_from_eating: 10.0,
         energy_yield_from_digestion: 1.0,
