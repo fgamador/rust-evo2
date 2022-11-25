5951d3a626bda416db3f3d3cea66d377f442a1e8
Franz Amador <franzamador@gmail.com>
2022-11-25 11:48:03 -0800

    f Add LinearlyGrowingFoodSource

--- src/food_sources.rs
+++ src/food_sources.rs
@@ -21,3 +21,37 @@ impl FoodSource for ConstantFoodSource {
         self.food_per_step
     }
 }
+
+struct LinearlyGrowingFoodSource {
+    next_food: F32Positive,
+    food_increase_per_step: F32Positive,
+}
+
+impl LinearlyGrowingFoodSource {
+    pub fn new(starting_food: F32Positive, food_increase_per_step: F32Positive) -> Self {
+        LinearlyGrowingFoodSource {
+            next_food: starting_food,
+            food_increase_per_step,
+        }
+    }
+}
+
+impl FoodSource for LinearlyGrowingFoodSource {
+    fn food_this_step(&mut self) -> F32Positive {
+        let result = self.next_food;
+        self.next_food += self.food_increase_per_step;
+        result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn linearly_growing_food_source_grows_linearly() {
+        let mut source = LinearlyGrowingFoodSource::new(100.0.into(), 10.0.into());
+        assert_eq!(source.food_this_step(), 100.0.into());
+        assert_eq!(source.food_this_step(), 110.0.into());
+    }
+}
